e158287d7c05f54747bac1836c59a6704bfce2e3
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-09 23:30:25 +0530

    login page done

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    <section className="login">
+      <div className="container">
+        <h2>Sign In</h2>
+        <form className="form login__form">
+          <p className="form__error-message">This is an error message</p>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          <button type="submit" className="btn primary">
+            Login
+          </button>
+          <small>
+            Don't have an account? <Link to={"/register"}>sign up</Link>
+          </small>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Login;
