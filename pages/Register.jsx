98cbcbd9904cd6e37262625aff06ec8546c84a75
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-09 23:30:34 +0530

    register page done

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  return (
+    <section className="register">
+      <div className="container">
+        <h2>Sign Up</h2>
+        <form className="form register__form">
+          <p className="form__error-message">This is an error message</p>
+          <input
+            type="text"
+            placeholder="username"
+            name="name"
+            value={userData.name}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          <button type="submit" className="btn primary">
+            Register
+          </button>
+          <small>
+            Already have an account? <Link to={"/login"}>sign in</Link>
+          </small>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Register;
