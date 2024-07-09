aa8016fb5b9dc3f683816209249bee6450596044
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-10 01:34:16 +0530

    dashboard update

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { DUMMY_POSTS } from "../src/data";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const [posts, setPosts] = useState(DUMMY_POSTS);
+  return (
+    <section className="dashboard">
+      {posts.length ? (
+        <div className="container dashboard__container">
+          {posts.map((posts) => {
+            return (
+              <article key={posts.id} className="dashboard__post">
+                <div className="dashboard__posts-info">
+                  <div className="dashboard__post-thumbnail">
+                    <img src={posts.thumbnail} alt="avatar" />
+                  </div>
+                  <h5>{posts.title}</h5>
+                </div>
+                <div className="dashboard__post-actions">
+                  <Link to={`/posts/${posts.id}`} className="btn sm">
+                    view
+                  </Link>
+                  <Link to={`/posts/${posts.id}`} className="btn sm primary">
+                    edit
+                  </Link>
+                  <Link to={`/posts/${posts.id}`} className="btn sm danger">
+                    delete
+                  </Link>
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      ) : (
+        <h2 className="center">You have no posts yet</h2>
+      )}
+    </section>
+  );
 }
 
 export default Dashboard;
