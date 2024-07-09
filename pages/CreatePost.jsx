24d82dc748a0acb058a818f41df9e4b46842d9e6
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-10 02:26:21 +0530

    create post done

--- pages/CreatePost.jsx
+++ pages/CreatePost.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function CreatePost() {
-  return <div>CreatePost</div>;
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("Uncategorized");
+  const [description, setDescription] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blackquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  const POST_CATEGORIES = [
+    "Uncategorized",
+    "Agriculture",
+    "Business",
+    "Education",
+    "Entertainment",
+    "Art",
+    "Investment",
+    "Weather",
+  ];
+
+  return (
+    <section className="create-post">
+      <div className="container">
+        <h2>Create Post</h2>
+        <p className="form__error-message">This is an error message</p>
+        <form className="form create-post__form">
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            autoFocus
+          />
+          <select
+            name="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            {POST_CATEGORIES.map((cat) => (
+              <option key={cat}>{cat}</option>
+            ))}
+          </select>
+          <ReactQuill
+            formats={formats}
+            modules={modules}
+            value={description}
+            onChange={setDescription}
+          />
+          <input
+            type="file"
+            onChange={(e) => setThumbnail(e.target.files[0])}
+            accept="png, jpg, jpeg"
+          />
+          <button type="submit" className="btn primary">
+            create
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default CreatePost;
