b41b7de40dd1f09c77aea34b4879495b08a0b855
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-10 01:10:47 +0530

    userProfile completed

--- pages/UserProfile.jsx
+++ pages/UserProfile.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../src/assets/images/avatar15.jpg";
+import { FaEdit } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 
 function UserProfile() {
-  return <div>UserProfile</div>;
+  const [avatar, setAvatar] = useState(Avatar);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  return (
+    <section className="profile">
+      <div className="container profile__container">
+        <Link to={`/myposts/zzz`}>My posts</Link>
+        <div className="profile__details">
+          <div className="avatar__wrapper">
+            <div className="profile__avatar">
+              <img src={avatar} alt="avatar" />
+            </div>
+            <form className="avatar__form">
+              <input
+                type="file"
+                name="avatar"
+                id="avatar"
+                accept="png, jpg, jpeg"
+                onChange={(e) => setAvatar(e.target.files[0])}
+              />
+              <label htmlFor="avatar">
+                <FaEdit />
+              </label>
+            </form>
+            <button className="profile__avatar-btn">
+              <FaCheck />
+            </button>
+          </div>
+          <h1>Sandesh Shrestha</h1>
+
+          <form className="form profile__form">
+            <p className="form__error-message">This is an error message</p>
+            <input
+              type="text"
+              placeholder="Full Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="currentPassword"
+              value={currentPassword}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="New Password"
+              value={newPassword}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className="btn primary">
+              Update details
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default UserProfile;
